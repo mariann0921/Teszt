373e109bc3eac27dab88c46e4d6742061089e2d6
Mariann <mariann.bogdan@petrik.hu>
2026-01-30 08:40:33 +0100

    valami hozzaadva az adat osztaly

--- Adat.cs
+++ Adat.cs
@@ -10,16 +10,18 @@ namespace Teszt
 	{
 		private string nev;
 		private int ar;
+		private double valami;
 
 		public Adat(string nev, int ar)
 		{
 			this.nev = nev;
 			this.ar = ar;
+			this.valami = 0;
 		}
 
 		public string Nev { get => nev; set => nev = value; }
 		public int Ar { get => ar; set => ar = value; }
-
+		public double Valami { get => valami; set => valami = value; }
 
 		public override string ToString()
 		{
